9f92314d2ba014f591f21c7664f648025249c763
Michał Czyż <mike@c2yz.com>
2025-10-25 22:22:27 +0200

    nvidia: suspend issues

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -1,7 +1,17 @@
 {
   hardware = {
     graphics.enable = true;
-    nvidia.open = true;
+
+    nvidia = {
+      open = true;
+
+      nvidiaSettings = true;
+
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
+    };
   };
 
   services.xserver.videoDrivers = ["nvidia"];
