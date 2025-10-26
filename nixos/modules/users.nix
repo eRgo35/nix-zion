6b7118dfc66ea8e51a502c8fb76b021e4a2d7d85
Michał Czyż <mike@c2yz.com>
2025-10-26 15:17:20 +0100

    ssh: added pubkeys

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -4,6 +4,10 @@
     isNormalUser = true;
     description = "Michał Czyż";
     extraGroups = ["networkmanager" "wheel" "docker"];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3xXO97oTwDMQhwlDcgeRgo+05urrSaKv/bvqvH2Wus mike@thor"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMCj2eJcNnzYW3O64LFunkDeQDPShD2oK/nivcwzSgwe u0_a670@localhost"
+    ];
     # shell = pkgs.nushell;
   };
 }
