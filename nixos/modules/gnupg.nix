0a116c41ebf2be02ddf6ce2de155b65e3b36cf79
Michał Czyż <mike@c2yz.com>
2025-12-28 20:46:49 +0100

    feat: longer timeouts for gpg

--- nixos/modules/gnupg.nix
+++ nixos/modules/gnupg.nix
@@ -4,8 +4,8 @@
     # pinentryPackage = with pkgs; pinentry-all;
     enableSSHSupport = true;
     settings = {
-      default-cache-ttl = 3600;
-      max-cache-ttl = 14400;
+      default-cache-ttl = 14400;
+      max-cache-ttl = 28800;
     };
   };
 }
